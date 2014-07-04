c51ff1edc0582529ef3f19dc58ba96b589e3bafe
Febin John James <jamesfebin@gmail.com>
2014-07-05 01:11:15 +0530

    setup

---
+++ homeAutomation.js
@@ -0,0 +1,52 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to homeAutomation.";
+  };
+
+Template.hello.welcome = function ()
+{
+
+return "Welcome Arpith";
+};
+
+
+Template.rooms.rooms = function()
+{
+
+return Rooms.find();
+}
+
+  Template.hello.events({
+    'click input': function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+Router.map(function () {
+  this.route('rooms', {
+    path: '/rooms',
+    waitOn: function () {
+
+    }
+  });
+});
+
+Router.map(function () {
+  this.route('hello', {
+    path: '/hello',
+    waitOn: function () {
+
+    }
+  });
+});
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+Rooms = new Meteor.Collection("rooms");
