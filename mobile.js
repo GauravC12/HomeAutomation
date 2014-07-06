4068f72d86fefbea7f54ded3b8ad7e1fbe4978e2
Gaurav Chouhan <gauravc12@gmail.com>
2014-07-06 10:45:34 +0530

    Changes to mobile.html

---
+++ mobile.js
@@ -0,0 +1,28 @@
+if(Meteor.isClient){
+	/*
+
+	Template.homescreen.events({
+		'click #calendarBtn': function (){
+			$('.phone').html($('#calendarDiv').html());
+		}
+	});
+
+	Template.homescreen.events({
+		'click #thermostatBtn' : function() {
+			$('.phone').html($('#thermostatDiv').html());
+		}
+	});
+
+	Template.homescreen.events({
+		'click #callBtn' : function() {
+			alert('not implemented');
+		}
+	});
+
+	Template.homescreen.events({
+		'click #backBtn' : function() {
+			$('.phone').html($('#homescreenDiv').html());
+		}
+	});*/
+
+}
